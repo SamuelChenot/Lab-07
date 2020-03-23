e67f7ec1ae9796eeb89a79f03a7285a619fc6f44
Samuel Chenot <samuelchenot@gmail.com>
2020-03-22 20:17:06 -0400

    I finished

---
+++ main.py
@@ -0,0 +1,8 @@
+userName = input("Hello! Please enter your facebook username!")
+
+userPassword = input("Hello! Please enter your facebook password!")
+
+if(userName.lower() == "samuel" and userPassword == "ProgrammingIsRocks!"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
